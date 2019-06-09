5a55b9bcdf2e462728b5ffa6bd767079a921b061
Adrian Roman <aroman_ro@yahoo.com>
2019-06-09 15:17:36 +0300

    Added Boole integration but SimpsonOneThird remains the one that's used, seems ok for the purpose.

--- DFTAtom/Integral.h
+++ DFTAtom/Integral.h
@@ -33,6 +33,35 @@ namespace DFT {
 			return sum * delta / 3.;
 		}
 
+		static double Boole(const double delta, const std::vector<double>& values)
+		{
+			assert(values.size() > 4);
+			assert(values.size() % 4 == 1);
+
+			double sum = 7. * (values.front() + values.back());
+
+			double sum32 = 0;
+			double sum12 = 0;
+			double sum14 = 0;
+
+			const int szm = static_cast<int>(values.size() - 1);
+			
+			for (int i = 1; i < szm; ++i)
+			{
+				sum32 += values[i++];
+
+				if (i < szm)
+				{
+					if (i % 4 == 0) sum14 += values[i];
+					else sum12 += values[i];
+				}
+			}
+
+			sum += 32. * sum32 + 12. * sum12 + 14 * sum14;
+
+			return sum * delta * 2. / 45.;
+		}
+
 	};
 
 }
