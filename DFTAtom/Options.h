5837e7ccc07a320911caa68e4945ff93014e8463
Adrian Roman <aroman_ro@yahoo.com>
2020-01-11 12:16:53 +0200

    Avoid double deletion if options is copied

--- DFTAtom/Options.h
+++ DFTAtom/Options.h
@@ -10,6 +10,31 @@ class Options
 public:
 	Options();
 
+	// avoid double deletion of m_fileconfig at destruction if copied
+	Options(const Options& other)
+		:
+		Z(other.Z),
+		MultigridLevels(other.MultigridLevels),
+		MaxR(other.MaxR),
+		deltaGrid(other.deltaGrid),
+		alpha(other.alpha),
+		m_fileconfig(nullptr)
+	{
+	}
+
+	Options& operator=(const Options& other)
+	{
+		Z = other.Z;
+		MultigridLevels = other.MultigridLevels;
+		MaxR = other.MaxR;
+		deltaGrid = other.deltaGrid;
+		alpha = other.alpha;
+		m_fileconfig = nullptr;
+
+		return *this;
+	}
+
+
 	void Load();
 	void Save();
 
